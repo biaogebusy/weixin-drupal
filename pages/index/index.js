125eb3d85a4eec82d83a4e3b2d9c958e5fa6a635
biaogebusy <349255833@qq.com>
2019-06-02 22:50:12 +0800

    feat: add api

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,26 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    articles: []
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
+  onLoad() {
+    this.getArticles();
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+
+  getArticles() {
+    wx.request({
+      url: 'https://api.zhaobg.com/jsonapi/node/article',
+      header: {
+        Accept: 'application/vnd.api+json'
+      },
+      success: (res) => {
+        const articles = res.data.data;
+        console.log(articles)
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          articles: articles
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
   }
 })
