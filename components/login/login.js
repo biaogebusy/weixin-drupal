75bd78885f657019784def2c225a361bd27dbb77
biaogebusy <349255833@qq.com>
2019-07-20 18:15:44 +0800

    fix: login

---
+++ components/login/login.js
@@ -0,0 +1,37 @@
+// components/login/login.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  onLoad: function(){
+    wx.getSetting({
+      success(res) {
+        console.log(res)
+      }
+    })
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onTapLogin(event) {
+      const loginDetail = {
+        userInfo: event.detail.userInfo
+      }
+      console.log(loginDetail)
+      this.triggerEvent('onLogin', loginDetail)
+    }
+  }
+})
